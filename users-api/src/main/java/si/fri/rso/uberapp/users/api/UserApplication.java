59f271201ecb56931a1b9498fcc922191117b4b1
glumacm <matjazglumac2ae@gmail.com>
2018-12-01 15:11:38 +0100

    Changed some definitions

--- users-api/src/main/java/si/fri/rso/uberapp/users/api/UserApplication.java
+++ users-api/src/main/java/si/fri/rso/uberapp/users/api/UserApplication.java
@@ -5,6 +5,6 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @RegisterService
-@ApplicationPath("/")
+@ApplicationPath("/v1")
 public class UserApplication extends Application {
 }
