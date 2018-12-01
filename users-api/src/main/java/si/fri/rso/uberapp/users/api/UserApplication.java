0630bb6ba7a5bd9771f99a5d49f3aac12854a9a5
glumacm <matjazglumac2ae@gmail.com>
2018-12-01 14:54:05 +0100

    Added REgister service to application path.

--- users-api/src/main/java/si/fri/rso/uberapp/users/api/UserApplication.java
+++ users-api/src/main/java/si/fri/rso/uberapp/users/api/UserApplication.java
@@ -1,8 +1,10 @@
 package si.fri.rso.uberapp.users.api;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@RegisterService
 @ApplicationPath("/")
 public class UserApplication extends Application {
 }
